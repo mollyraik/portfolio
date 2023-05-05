11a2604985266c0c2a93f00d4765e1530aa1f6cd
mollyraik <molly.raik@gmail.com>
2023-05-05 18:47:20 -0400

    wip: adds skill component

--- components/Skill.js
+++ components/Skill.js
@@ -1,11 +1,13 @@
-const Skill = ({ skill }) => {
+import Image from "next/image";
+
+const Skill = ({ skill, skillImg }) => {
   return (
     <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
       <div className="grid grid-cols-2 gap-4 justify-center items-center">
         <div className="m-auto">
           <Image
-            src="/../public/assets/skills/{skill}.png"
-            alt="/"
+            src={skillImg}
+            alt={skill}
             width="64"
             height="64"
           />
