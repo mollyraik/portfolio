74dd41b2f2d6c4d59dc97ba27fa1af09063e26d5
mollyraik <molly.raik@gmail.com>
2023-05-05 19:40:08 -0400

    wip: beginning of project page

--- pages/downtomatch.js
+++ pages/downtomatch.js
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import dtm from "../public/assets/projects/dtm.png";
+
 const downToMatch = () => {
   return (
-    <div>
-      <h1>Down to Match</h1>
+    <div className="w-full">
+        <div className="w-screen h-[30vh] lg:h-[40vh] relative">
+            <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10"></div>
+            <Image className="absolute z-1" layout='fill' objectFit="cover" src={dtm} alt='Down to Match'/>
+            <div className="absolute z-10 p-2 top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white">
+                <h2 className="py-2">Down to Match</h2>
+                <h3>React JS / Bootstrap / Firebase / Socket.io </h3>
+            </div>
+        </div>
+        <div className="max-w-[1240px mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
+            <div className="col-span-4">
+                <p>Project</p>
+                <h2>Overview</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem excepturi corrupti possimus animi mollitia commodi totam dolores repudiandae eos? Aut quaerat ipsa aspernatur perferendis et reprehenderit qui fuga cum corrupti.</p>
+            </div>
+
+        </div>
     </div>
   );
 }
