74a758c525b2166b9d5df14725911ed2cebd05d2
mollyraik <molly.raik@gmail.com>
2023-05-05 18:49:25 -0400

    wip: initial styles for projects section

--- components/Projects.js
+++ components/Projects.js
@@ -1,18 +1,28 @@
+import Image from "next/image";
+import dtm from "../public/assets/projects/dtm.png";
+import wibl from "../public/assets/projects/wibl.png";
+import classfolio from "../public/assets/projects/classfolio.png";
+import formula1v1 from "../public/assets/projects/formula1v1.png";
+import Link from "next/link";
+import Project from "./Project";
+
 const Projects = () => {
-    return (
-        <div className="w-full">
-            <div className="max-w-[1240px] mx-auto px-2 py-16">
-                <p className="text-xl tracking-widest uppercase text-[#AFD6DF]">Projects</p>
-                <h2 className="py-4">What I've Built</h2>
-                <div className="grid md:grid-cols-2 gap-8">
-                    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to to-[#709dff]">
-
-                    </div>
-                </div>
-
-            </div>
+  return (
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#AFD6DF]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <Project title='Down to Match' image={dtm} projectUrl='/downtomatch' tech='React JS' />
+          <Project title='Will I Be Late?' image={wibl} projectUrl='/willibelate' tech='Django/Python' />
+          <Project title='Classfolio' image={classfolio} projectUrl='/classfolio' tech='Node/Express' />
+          <Project title='Formula 1 v 1' image={formula1v1} projectUrl='/formula1v1' tech='jQuery/HTML/CSS' />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
