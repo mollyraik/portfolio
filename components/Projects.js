650fd2a5a3e50af30f217099defcf76660d014c4
mollyraik <molly.raik@gmail.com>
2023-05-06 14:29:10 -0400

    wip: fixes smooth scroll location

--- components/Projects.js
+++ components/Projects.js
@@ -8,7 +8,7 @@ import Project from "./Project";
 
 const Projects = () => {
   return (
-    <div id="projects" className="w-full">
+    <div id="projects" className="w-full py-16">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[#AFD6DF]">
           Projects
