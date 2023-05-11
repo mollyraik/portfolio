25beca7192b33934bca042a6c62345862e88c1ab
mollyraik <molly.raik@gmail.com>
2023-05-10 22:32:23 -0400

    wip: adds about me text

--- components/About.js
+++ components/About.js
@@ -4,9 +4,12 @@ import molly from "../public/assets/molly1.png";
 
 const About = () => {
   return (
-    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16 relative mt-5">
+    <div
+      id="about"
+      className="w-full md:h-screen p-2 flex items-center py-16 relative mt-5"
+    >
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 py-8">
-      <div className="w-full h-auto m-auto shadow-lg shadow-gray-100 dark:shadow-gray-600 rounded-xl border-2 border-peach flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+        <div className="w-full h-auto m-auto shadow-lg shadow-gray-100 dark:shadow-gray-600 rounded-xl border-2 border-peach flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
           <Image
             src={molly}
             height={400}
@@ -19,26 +22,37 @@ const About = () => {
             About
           </p>
           <h2 className="py-4 ">Who I Am</h2>
-          <p className="py-2 text-gray-600 dark:text-gray-300">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-            veniam repudiandae in dolor. Expedita ex, aliquam velit enim fugit
-            fuga iste, esse earum ab itaque culpa deleniti. Dolor, quaerat.
-            Quibusdam?
+          <p className="py-2 text-gray-00 dark:text-gray-300">
+            Hey there! 👋 I'm Molly, a software developer and former teacher
+            with a passion for education and technology. I've taught
+            kindergarten and worked as a learning specialist, where I honed my
+            skills in creating inclusive and engaging learning environments.
           </p>
           <p className="py-2 text-gray-600 dark:text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-            debitis placeat odit autem aliquam sunt mollitia facere itaque in
-            architecto corporis, recusandae eaque perferendis possimus
-            repellendus, necessitatibus commodi explicabo libero!
+            After transitioning into software development, I discovered a new
+            avenue for making a positive impact. From coding innovative projects
+            to exploring various programming languages and technologies, I
+            thrive on the dynamic nature of this field.
           </p>
           <p className="py-2 text-gray-600 dark:text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-            deserunt quasi placeat dolorem molestias! Quos beatae libero totam
-            earum minima eos accusamus ipsam nemo impedit ab. Nobis incidunt
-            ratione reprehenderit.
+            As a native New Yorker, I embrace diversity
+            and creativity as invaluable assets. They've shaped my collaborative
+            problem-solving skills and fostered an open-minded approach to my
+            work.
           </p>
-          <Link href='/#projects' scroll={false}>
-              <p className="py-2 text-orange dark:text-orange underline cursor-pointer">Check out some of my latest projects</p>
+          <p className="py-2 text-gray-600 dark:text-gray-300">
+            As a developer, I value continuous learning, collaboration, and
+            creating user-centered solutions. Drawing from my background in
+            education, I aim to bridge gaps, making technology accessible and
+            impactful for all. I'm excited to connect with like-minded
+            developers and innovators. If you're interested in exploring my work
+            or discussing shared interests, feel free to reach out or check out
+            my portfolio.
+          </p>
+          <Link href="/#projects" scroll={false}>
+            <p className="py-2 text-orange dark:text-orange underline cursor-pointer">
+              Check out some of my latest projects
+            </p>
           </Link>
         </div>
       </div>
